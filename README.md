2d6bfe2bafd36b681dc2b8f476d5901a0cad7ccd
Jackson <jackson@sneagan.com>
2013-02-20 22:02:19 -0500

    Generalized output and style update

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 SimplerShowNotes
 ================
 
-A quick and dirty JS script to create a new RSS &lt;item&gt; for Podcasts (some assembly required). It can create show notes so you don't have to. You can find an example live [here](http://bookbytes.tv/simpler-show-notes-example). The mobile version has currently only been tested on iOS devices.
+A quick form with a script to create a new RSS &lt;item&gt; for Podcasts. It can create show notes so you don't have to. You can find a fully functioning example live [here](http://simplershownotes.com).
 
 I wrote up this little script to get podcast information from a form and output code to speed up the production of RSS podcast episodes. It supports URL schemes to recieve data from [Drafts](http://agiletortoise.com/drafts) in addition to direct input. It **will not** write the entire RSS feed for you. If you need a wireframe to start with, look [here](http://www.podcast411.com/howto_1.html). 
 
-Podcast feeds are pretty standard so the XML portion of the script should work well for most podcasts with minimal editing. It also supports the output of HTML for a site, but that will vary much more from podcast to podcast and will require more customization. The nice thing about doing it this way is that you can very easily add in dropdowns if you work on multiple podcasts, or make more information in the JS file fixed instead of variable if you find yourself filling in the same information a lot. Go nuts!
+Podcast feeds are pretty standard so the XML output should work well for most podcasts with little to no editing. If you know Javascript, you can change some of the variables to constants to avoid constantly filling in the same info, or even output HTML for your site. Go nuts!
 
 You can get a handle on the Drafts App support with the live example. In Drafts, go to Settings and add a new URL Action. Call it whatever you like. To fill in the Summary and Notes Text sections with the first and second lines you write in Drafts, the URL scheme is:
 
-    http://bookbytes.tv/simpler-show-notes-example/?summarytext=[[title]]&notestext=[[body]]
+    http://simplershownotes.com/?summarytext=[[title]]&notestext=[[body]]
 
 To fill in the Show Notes Text and Show Notes URL section with the first and second lines you write in Drafts, the URL scheme is:
 
-    http://bookbytes.tv/simpler-show-notes-example/?notestext=[[title]]&notesurl=[[body]]
+    http://simplershownotes.com/?notestext=[[title]]&notesurl=[[body]]
 
-The Drafts support could be explanded to fill in any or all of the fields in the form, but you'd have to set up your own fill-in rules, as Drafts only supports first and second line export syntax.
+The Drafts support could be explanded to fill in any or all of the fields in the form, but you'd have to set up your own fill-in rules and syntax, as Drafts only supports exporting the first and second lines.
--- js/simplerShowNotes.js
+++ js/simplerShowNotes.js
@@ -10,7 +10,7 @@ function ifSummaryFirst(pageURL) {
     var theSummary, theText, summaryDraftsArray, decodedText, remHTTP;
     
     decodedText = decodeURIComponent(pageURL);
-    remHTTP = decodedText.replace("http://your-podcast-url/newepisode/?summarytext=", "");
+    remHTTP = decodedText.replace("http://simplershownotes.com/?summarytext=", "");
     notesText = remHTTP.replace("&notestext=", ";");
     summaryDraftsArray = notesText.split(";");
     
@@ -29,7 +29,7 @@ function ifNotesFirst(pageURL) {
     var theNotesText, decodedText, remHTTP, notesURL, notesDraftsArray;
     
     decodedText = decodeURIComponent(pageURL);
-    remHTTP = decodedText.replace("http://your-podcast-url/newepisode/?notestext=", "");
+    remHTTP = decodedText.replace("http://simplershownotes.com/?notestext=", "");
     notesURL = remHTTP.replace("&notesurl=", ";");
     notesDraftsArray = notesURL.split(";");
     
@@ -45,13 +45,13 @@ function ifNotesFirst(pageURL) {
 
 function episodeProcessor(form) {
     // All variables
-    var podcastSite, itunesAuthorDisplay, itunesExplicit, itunesSubtitle, currentTime, day, date, year, rawHours, rawMinutes, rawSeconds, hours, minutes, seconds, monthNames, dayNames, rssTime, newTitle, newBook, bookNameArray, bookFirstWord, newAuthor, newSummary, episodeNumber, newShowNotesTextString, newShowNotesURLString, phl, pml, psl, podcastLength, showNotesIterator, finalNotesArray, hostsArray, finalHostsArray, showNotesTextArray, showNotesURLArray, showNotesNumber, hostsNum, hostsIterator, host1, host2, host3, host4, host1Value, host2Value, host3Value, host4Value, mediumRadio, hostsToWrite, notesToWrite, newEpisodeHTML, newEpisodeXML, episodeKeywords, episodeFileSize, showHide;
+    var podcastSite, itunesAuthor, itunesSubtitle, currentTime, day, date, year, rawHours, rawMinutes, rawSeconds, hours, minutes, seconds, monthNames, dayNames, rssTime, newTitle, newBook, bookNameArray, bookFirstWord, newAuthor, newSummary, episodeNumber, newShowNotesTextString, newShowNotesURLString, phl, pml, psl, podcastLength, showNotesIterator, finalNotesArray, hostsArray, finalHostsArray, showNotesTextArray, showNotesURLArray, showNotesNumber, hostsNum, hostsIterator, explicitRadio, hostsToWrite, notesToWrite, newEpisodeHTML, newEpisodeXML, episodeKeywords, episodeFileSize, showHide, mediaSourceURL, hostsURLArray, hostsTextArray;
 
 // EDIT THIS
-    podcastSite = "thePodcast"; // A link to your podcast site
-    itunesAuthorDisplay = "Space Butterfly" // Who goes in the iTunes author box?
-    itunesSubtitle = "Podcasts Rock" // Self explanatory, no?
-    itunesExplicit = "Maybe" // Only accepts Yes or No.
+    podcastSite = document.getElementById("podcastSite").value;
+    itunesAuthor = document.getElementById("itunesAuthor").value;
+    itunesSubtitle = document.getElementById("itunesSubtitle").value;
+    mediaSourceURL = document.getElementById("mediaSourceURL").value;
 // END
 
     // This gets the current time and sets the variable to use later. Was using + currentTime.getTimezoneOffset() to get UTC offset, but was off by 2 hours. Set it manually or fix as you like. It's "-0500" in rssTime.
@@ -75,76 +75,62 @@ function episodeProcessor(form) {
 //END
 
 // EDIT THIS
-    // Set mediumRadio so that we can determine the new title in the next section. If your podcast is not about books, you'll need to edit this as well as the radio input on the HTML page.
-    if (document.getElementById("bookSelected").checked === true) {
-        mediumRadio = "Book";
-    } else if (document.getElementById("comicSelected").checked ===true) {
-        mediumRadio = "Comic";
+    // Set mediumRadio
+    if (document.getElementById("explicitSelected").checked === true) {
+        explicitRadio = "Yes";
+    } else if (document.getElementById("explicitDeselected").checked ===true) {
+        explicitRadio = "No";
     };
 // END
 
 // EDIT THIS
-    // Create initial variables for the functions to follow. You podcast may need other form input. Remember to change both the HTML form and these variables
-    newTitle = mediumRadio + " - " + form.bookTitle.value;
-    newBook = form.bookTitle.value;
-    newAuthor = form.authorName.value;
+    // Create initial variables for the functions to follow.
+    newTitle = document.getElementById("itunesTitle").value;
     newSummary = form.newEpisodeSummary.value;
     episodeNumber = form.newEpisodeNumber.value;
     newShowNotesTextString = form.newNotesText.value;
     newShowNotesURLString = form.newNotesURL.value;
+    newHostsTextString = form.hostsText.value;
+    newHostsURLString = form.hostsURL.value;
     episodeFileSize = form.newFileSize.value;
     episodeKeywords = form.newKeywords.value;
     phl = form.lengthHours.value;
     pml = form.lengthMinutes.value;
     psl = form.lengthSecond.value;
-    bookNameArray = newBook.split(" ");
 // END
 
 // EDIT THIS
     // This handles the hosts. Remember to make sure your changes here are reflected in the form.
-    host1 = document.getElementById("host1").checked;
-    host2 = document.getElementById("host2").checked;
-    host3 = document.getElementById("host3").checked;
-    host4 = document.getElementById("host4").checked;
-    host1Value = document.getElementById("host1").value;
-    host2Value = document.getElementById("host2").value;
-    host3Value = document.getElementById("host3").value;
-    host4Value = document.getElementById("host4").value;
+    // Make some Arrays
     hostsArray = new Array();
     finalHostsArray = new Array();
     finalNotesArray = new Array();
-
-    // Populate array with hosts
-    if (host1) {hostsArray.push(host1Value); };
-    if (host2) {hostsArray.push(host2Value); };
-    if (host3) {hostsArray.push(host3Value); };
-    if (host4) {hostsArray.push(host4Value); };
-//END
-
-    hostsNum = hostsArray.length;
+    // Populate Hosts arrays
+    hostsTextArray = newHostsTextString.split(", ");
+    hostsURLArray = newHostsURLString.split(", ");
+    // Make sure there are an equal number of show note text and urls added
+    if (hostsTextArray.length === hostsURLArray.length) {
+        hostsNum = hostsTextArray.length;
+    } else {
+        alert("Your show notes links and text don't match up");
+    };
     hostsIterator = -1;
     showNotesIterator = -1;
-
+    // Populate new Show Notes array
+    for (i = 0; i < hostsNum; i++) {
+        hostsIterator = hostsIterator + 1;
+        hostsToWrite = "&amp;lt;p&amp;gt;&amp;lt;a href=\"" + hostsURLArray[hostsIterator] + "\"&amp;gt;" + hostsTextArray[hostsIterator] + "&amp;lt;/a&amp;gt;&amp;lt;/p&amp;gt;";
+        finalHostsArray.push(hostsToWrite);
+    };
     // Populate Show Notes arrays
     showNotesTextArray = newShowNotesTextString.split(", ");
     showNotesURLArray = newShowNotesURLString.split(", ");
-
     // Make sure there are an equal number of show note text and urls added
     if (showNotesTextArray.length === showNotesURLArray.length) {
         showNotesNumber = showNotesTextArray.length;
     } else {
-        console.log("Your show notes links and text don't match up");
+        alert("Your show notes links and text don't match up");
     };
-
-// EDIT THIS
-    // Populate new Hosts array. It currently links to App.net accounts, but you can change it to whatever.
-    for (i = 0; i < hostsNum; i++) {
-    hostsIterator = hostsIterator + 1;
-    hostsToWrite = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;a href=\"https://alpha.app.net/" + hostsArray[hostsIterator] + "\"&gt;" + hostsArray[hostsIterator] + "&lt;/a&gt;<br>";
-        finalHostsArray.push(hostsToWrite);
-    };
-// END
-
     // Populate new Show Notes array
     for (i = 0; i < showNotesNumber; i++) {
         showNotesIterator = showNotesIterator + 1;
@@ -153,7 +139,6 @@ function episodeProcessor(form) {
     };
 
     //Set variable to write audio file url and length
-    if (bookNameArray.length > 1) {bookFirstWord = bookNameArray[0]} else {bookFirstWord = newBook};
     if (phl === "undefined") {phl = "0"};
     if (phl.length < 2) {phl = "0" + phl};
     if (pml === "undefined") {pml = "0"};
@@ -163,15 +148,12 @@ function episodeProcessor(form) {
     podcastLength = phl + ":" + pml + ":" + psl;
 
 // EDIT THIS
-    // This stuff is no fun and ugly. The most editing you'll have to do is with the HTML. You can delete the whole newEpisodeHTML variable if you don't want it written up for you.
-    newEpisodeHTML = "&lt;div class=\"entry episode\" id=\"episode-" + episodeNumber + "\"&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&lt;div class=\"header\"&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;h3 class=\"number\"&gt;" + episodeNumber + "&lt;/h3&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;h3 class=\"title\"&gt;&lt;a href=\"episodes/" + episodeNumber + "\"&gt;" + newTitle + "&lt;/a&gt;&lt;/h3&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;h3 class=\"part\"&gt;by " + newAuthor + "&lt;/h3&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br>&lt;!-- .header ends --&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&lt;div class=\"meta-data\"&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;span&gt;Hosts:&lt;/span&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div class=\"hosts\"&gt;<br>" + finalHostsArray.sort().join(" ") + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br>&lt;!-- .speakers ends --&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br>&lt;!-- .meta-data ends --&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&lt;div class=\"text\"&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;p&gt;" + newSummary + "&lt;/p&gt;<br>&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br>&lt;!-- .text ends --&gt;<br>&lt;/div&gt;<br>&lt;!-- #episode-" + episodeNumber + " ends --&gt;<br>";
-
-    newEpisodeXML = "&lt;item&gt;<br>&nbsp;&nbsp;&lt;title&gt;Ep. " + episodeNumber + ": " + newTitle + "&lt;/title&gt;<br>&nbsp;&nbsp;&lt;link>" + podcastSite + "&lt;/link&gt;<br>&nbsp;&nbsp;&lt;guid&gt;000" + episodeNumber + "&lt;/guid&gt;<br>&nbsp;&nbsp;&lt;description&gt;&amp;lt;p&amp;gt;" + newSummary + "&amp;lt;/p&amp;gt;" + finalNotesArray.sort().join("") + "&lt;/description&gt;<br>&lt;enclosure url=\"https://directory-to-episodes/ep" + episodeNumber + "-" + bookFirstWord + ".m4a\" length=\"" + episodeFileSize + "\" type=\"audio/mpeg\"&gt;<br>&nbsp;&nbsp;&lt;category&gt;Podcasts&lt;/category&gt;<br>&nbsp;&nbsp;&lt;pubDate&gt;" + rssTime + "&lt;/pubDate&gt;<br>&nbsp;&nbsp;&lt;itunes:author&gt;" + itunesAuthorDisplay + "&lt;/itunes:author&gt;<br>&nbsp;&nbsp;&lt;itunes:explicit&gt;" + itunesExplicit + "&lt;/itunes:explicit&gt;<br>&nbsp;&nbsp;&lt;itunes:subtitle&gt;" + itunesSubtitle + "&lt;/itunes:subtitle&gt;<br>&nbsp;&nbsp;&lt;itunes:summary&gt;&amp;lt;p&amp;gt;" + newSummary + "&amp;lt;/p&amp;gt; " + finalNotesArray.sort().join("") + "&lt;/itunes:summary&gt;<br>&nbsp;&nbsp;&lt;itunes:duration&gt;" + podcastLength + "&lt;/itunes:duration&gt;<br>&nbsp;&nbsp;&lt;itunes:keywords&gt;" + episodeKeywords + "&lt;/itunes:keywords&gt;<br>&lt;/item&gt;";
+    // This stuff is no fun and ugly.
+    newEpisodeXML = "&lt;item&gt;<br>&nbsp;&nbsp;&lt;title&gt;" + newTitle + "&lt;/title&gt;<br>&nbsp;&nbsp;&lt;link>" + podcastSite + "&lt;/link&gt;<br>&nbsp;&nbsp;&lt;guid&gt;000" + episodeNumber + "&lt;/guid&gt;<br>&nbsp;&nbsp;&lt;description&gt;&amp;lt;p&amp;gt;" + newSummary + "&amp;lt;/p&amp;gt;&lt;p&gt;" + finalNotesArray.join("") + "&lt;/p&gt;" + finalHostsArray.join("") + "&lt;/description&gt;<br>&lt;enclosure url=\"" + mediaSourceURL + "\" length=\"" + episodeFileSize + "\" type=\"audio/mpeg\"&gt;<br>&nbsp;&nbsp;&lt;category&gt;Podcasts&lt;/category&gt;<br>&nbsp;&nbsp;&lt;pubDate&gt;" + rssTime + "&lt;/pubDate&gt;<br>&nbsp;&nbsp;&lt;itunes:author&gt;" + itunesAuthor + "&lt;/itunes:author&gt;<br>&nbsp;&nbsp;&lt;itunes:explicit&gt;" + explicitRadio + "&lt;/itunes:explicit&gt;<br>&nbsp;&nbsp;&lt;itunes:subtitle&gt;" + itunesSubtitle + "&lt;/itunes:subtitle&gt;<br>&nbsp;&nbsp;&lt;itunes:summary&gt;&amp;lt;p&amp;gt;" + newSummary + "&amp;lt;/p&amp;gt; " + finalNotesArray.join("") + "&lt;/p&gt;" + finalHostsArray.join("") + "&lt;/itunes:summary&gt;<br>&nbsp;&nbsp;&lt;itunes:duration&gt;" + podcastLength + "&lt;/itunes:duration&gt;<br>&nbsp;&nbsp;&lt;itunes:keywords&gt;" + episodeKeywords + "&lt;/itunes:keywords&gt;<br>&lt;/item&gt;";
 // END
     
     showHide = document.getElementById("outputContainer");
     showHide.style.display = "block";
     document.getElementById("xmlOutput").innerHTML = newEpisodeXML;
-    document.getElementById("htmlOutput").innerHTML = newEpisodeHTML;
-    document.getElementById("submitButton").scrollIntoView();
+    document.getElementById("newKeywords").scrollIntoView();
 };
